0a0609d93cfe301c412265c5a4898d9fab3d8c4c
svyatoslav <yoko11.06.92@yandex.ru>
2020-11-05 11:55:24 +0400

    add cart

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
+from cart.forms import CartAddProductForm
 from shop.models import Product
 from shop.services.db_model_service import get_available_products
 
@@ -12,6 +13,8 @@ def product_list(request):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop/product/detail.html', {'product': product})
+    cart_product_form = CartAddProductForm()
+    data = {'product': product, 'cart_product_form': cart_product_form}
+    return render(request, 'shop/product/detail.html', context=data)
 
 
