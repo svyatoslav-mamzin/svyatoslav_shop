2b5d08cef716d871e7e28f4d6824e94ed2a8c151
svyatoslav <yoko11.06.92@yandex.ru>
2020-11-06 11:42:37 +0400

    add model cart

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+class OrderItemInline(admin.TabularInline):
+    model = CartItem
+    raw_id_fields = ['product']
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'paid']
+    list_filter = ['paid']
+    inlines = [OrderItemInline]
+
+
+admin.site.register(CartItem)
--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import User
 from django.db import models
+from shop.models import Product
+from decimal import Decimal
 
-# Create your models here.
+
+
+class Cart(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    paid = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return 'Cart {}'.format(self.id)
+
+    def get_total_cost(self):
+        total_cost = sum(item.get_cost() for item in self.items.all())
+        return total_cost
+
+
+class CartItem(models.Model):
+    order = models.ForeignKey(Cart,
+                              related_name='items',
+                              on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,
+                                related_name='order_items',
+                                on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
