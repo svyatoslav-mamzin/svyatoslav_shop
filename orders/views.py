7ecc2d4c4002bc0a32033a66a9153fdfec4af338
svyatoslav <yoko11.06.92@yandex.ru>
2020-11-10 14:54:27 +0400

    fix

--- orders/views.py
+++ orders/views.py
@@ -6,6 +6,9 @@ from .models import Order, OrderItem
 from .tasks import order_created
 from loguru import logger
 
+logger.add("logs/orders/log", format="{time} {level} {message}", level="ERROR",
+           rotation="10 MB", compression='zip')
+
 @logger.catch
 def order_create(request):
     cart = Cart(request)
@@ -18,6 +21,7 @@ def order_create(request):
         list_items.append(OrderItem(order=order, product=item.product,
                                     price=item.price, quantity=item.quantity))
     OrderItem.objects.bulk_create(list_items)
+    logger.info(f"заказ №{order.id} создан")
     html_text = html_creater(request.user.username, order, cart)
     order_created.delay(html_text, order.id)
     cart.clear()
